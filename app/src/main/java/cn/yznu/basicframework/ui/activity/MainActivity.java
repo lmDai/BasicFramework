12473f3c9c362a78eb2497670a8d0572c7777db4
lmDai <lmDaicq@qq.com>
2018-07-30 18:31:54 +0800

    重构初始化

--- app/src/main/java/cn/yznu/basicframework/ui/activity/MainActivity.java
+++ app/src/main/java/cn/yznu/basicframework/ui/activity/MainActivity.java
@@ -1,55 +1,43 @@
 package cn.yznu.basicframework.ui.activity;
 
 import android.os.Bundle;
-import android.support.annotation.IntRange;
 import android.support.annotation.Nullable;
 import android.view.KeyEvent;
-import android.widget.FrameLayout;
 
-import com.ashokvarma.bottomnavigation.BottomNavigationBar;
-import com.ashokvarma.bottomnavigation.BottomNavigationItem;
-
-import butterknife.BindView;
 import cn.yznu.basicframework.R;
 import cn.yznu.basicframework.base.BaseActivity;
-import cn.yznu.basicframework.ui.fragment.IndexFragment;
-import cn.yznu.basicframework.ui.fragment.SecondFragment;
-import cn.yznu.basicframework.ui.fragment.ThreeFragment;
-import cn.yznu.common.toasthelper.TastyToast;
-import cn.yznu.common.toasthelper.ToastHelper;
-import me.yokeyword.fragmentation.SupportFragment;
 
 
 public class MainActivity extends BaseActivity {
-    @BindView(R.id.bottom_navigation_bar)
-    BottomNavigationBar bottomNavigationBar;
-    @BindView(R.id.content)
-    FrameLayout rlTabContainer;
-    private Long firstTime = 0L;
-    private static final int INDEX = 0;
-    private static final int SECOND = 1;
-    private static final int THREE = 2;
-    private SupportFragment[] mFragments = new SupportFragment[3];
-    private int mSelectPosition = 0, mCurrentPosition = 0;
+//    @BindView(R.id.bottom_navigation_bar)
+//    BottomNavigationBar bottomNavigationBar;
+//    @BindView(R.id.content)
+//    FrameLayout rlTabContainer;
+//    private Long firstTime = 0L;
+//    private static final int INDEX = 0;
+//    private static final int SECOND = 1;
+//    private static final int THREE = 2;
+//    private SupportFragment[] mFragments = new SupportFragment[3];
+//    private int mSelectPosition = 0, mCurrentPosition = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (savedInstanceState == null) {
-            mFragments[INDEX] = IndexFragment.newInstance();
-            mFragments[SECOND] = SecondFragment.newInstance();
-            mFragments[THREE] = ThreeFragment.newInstance();
-            loadMultipleRootFragment(R.id.content, INDEX,
-                    mFragments[INDEX],
-                    mFragments[SECOND],
-                    mFragments[THREE]);
-        } else {
-            // 这里库已经做了Fragment恢复,所有不需要额外的处理了, 不会出现重叠问题
-            // 这里我们需要拿到mFragments的引用,也可以通过getChildFragmentManager.getFragments()自行进行判断查找(效率更高些),用下面的方法查找更方便些
-            mFragments[INDEX] = findFragment(IndexFragment.class);
-            mFragments[SECOND] = findFragment(SecondFragment.class);
-            mFragments[THREE] = findFragment(ThreeFragment.class);
-        }
+//        if (savedInstanceState == null) {
+//            mFragments[INDEX] = IndexFragment.newInstance();
+//            mFragments[SECOND] = SecondFragment.newInstance();
+//            mFragments[THREE] = ThreeFragment.newInstance();
+//            loadMultipleRootFragment(R.id.content, INDEX,
+//                    mFragments[INDEX],
+//                    mFragments[SECOND],
+//                    mFragments[THREE]);
+//        } else {
+//            // 这里库已经做了Fragment恢复,所有不需要额外的处理了, 不会出现重叠问题
+//            // 这里我们需要拿到mFragments的引用,也可以通过getChildFragmentManager.getFragments()自行进行判断查找(效率更高些),用下面的方法查找更方便些
+//            mFragments[INDEX] = findFragment(IndexFragment.class);
+//            mFragments[SECOND] = findFragment(SecondFragment.class);
+//            mFragments[THREE] = findFragment(ThreeFragment.class);
+//        }
     }
 
     @Override
@@ -64,49 +52,49 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        initBottomNavigationBar(mSelectPosition);
+//        initBottomNavigationBar(mSelectPosition);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
-            if (System.currentTimeMillis() - firstTime > 2000) {
-                ToastHelper.showToast(mContext, "再次点击退出程序", TastyToast.INFO);
-                firstTime = System.currentTimeMillis();
-            } else {
-                finish();
-                System.exit(0);
-            }
-            return true;
-        }
+//        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+//            if (System.currentTimeMillis() - firstTime > 2000) {
+//                ToastHelper.showToast(mContext, "再次点击退出程序", TastyToast.INFO);
+//                firstTime = System.currentTimeMillis();
+//            } else {
+//                finish();
+//                System.exit(0);
+//            }
+//            return true;
+//        }
         return super.onKeyDown(keyCode, event);
     }
 
-    private void initBottomNavigationBar(@IntRange(from = 0, to = 3) int position) {
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_index)).setInactiveIconResource(R.mipmap.ic_launcher_round));
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_message)).setInactiveIconResource(R.mipmap.ic_launcher_round));
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_contact)).setInactiveIconResource(R.mipmap.ic_launcher_round));
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_mine)).setInactiveIconResource(R.mipmap.ic_launcher_round));
-
-        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
-        bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
-        bottomNavigationBar.setActiveColor(R.color.colorAccent);
-        bottomNavigationBar.setInActiveColor(R.color.colorBgDark);
-        bottomNavigationBar.setFirstSelectedPosition(position);
-        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.SimpleOnTabSelectedListener() {
-            @Override
-            public void onTabSelected(int position) {
-                doOnTabSelected(position);
-            }
-        });
-        bottomNavigationBar.setBarBackgroundColor(R.color.colorWhite);
-        bottomNavigationBar.initialise();
-        bottomNavigationBar.setAutoHideEnabled(false);
-    }
-
-    private void doOnTabSelected(int position) {
-        mSelectPosition = position;
-        showHideFragment(mFragments[position], mFragments[mCurrentPosition]);
-        mCurrentPosition = position;
-    }
+//    private void initBottomNavigationBar(@IntRange(from = 0, to = 3) int position) {
+//        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_index)).setInactiveIconResource(R.mipmap.ic_launcher_round));
+//        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_message)).setInactiveIconResource(R.mipmap.ic_launcher_round));
+//        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_contact)).setInactiveIconResource(R.mipmap.ic_launcher_round));
+//        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.ic_launcher, getString(R.string.str_mine)).setInactiveIconResource(R.mipmap.ic_launcher_round));
+//
+//        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+//        bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+//        bottomNavigationBar.setActiveColor(R.color.colorAccent);
+//        bottomNavigationBar.setInActiveColor(R.color.colorBgDark);
+//        bottomNavigationBar.setFirstSelectedPosition(position);
+//        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.SimpleOnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(int position) {
+//                doOnTabSelected(position);
+//            }
+//        });
+//        bottomNavigationBar.setBarBackgroundColor(R.color.colorWhite);
+//        bottomNavigationBar.initialise();
+//        bottomNavigationBar.setAutoHideEnabled(false);
+//    }
+//
+//    private void doOnTabSelected(int position) {
+//        mSelectPosition = position;
+//        showHideFragment(mFragments[position], mFragments[mCurrentPosition]);
+//        mCurrentPosition = position;
+//    }
 }
---
+++ common/src/main/java/cn/yznu/common/widget/WrapContentViewPager.java
@@ -0,0 +1,79 @@
+package cn.yznu.common.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * 作者：uiho_mac
+ * 时间：2018/7/30
+ * 描述：高度自适应的viewpager
+ * 版本：1.0
+ * 修订历史：
+ */
+public class WrapContentViewPager extends ViewPager {
+    private int current;
+    private int height = 0;
+    private boolean scrollable = true;//是否可以滑动
+
+    public WrapContentViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public WrapContentViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        if (getChildCount() > current) {
+            View child = getChildAt(current);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            height = h;
+
+        }
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public void resetHeight(int current) {
+        this.current = current;
+        if (getChildCount() > current) {
+            CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) getLayoutParams();
+            if (layoutParams == null) {
+                layoutParams = new CollapsingToolbarLayout.LayoutParams(CollapsingToolbarLayout.LayoutParams.MATCH_PARENT, height);
+            } else {
+                layoutParams.height = height;
+            }
+            setLayoutParams(layoutParams);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (!scrollable) {
+            return true;
+        }
+        return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return scrollable && super.onInterceptTouchEvent(ev);
+    }
+
+    public boolean isscrollable() {
+        return scrollable;
+    }
+
+    public void setscrollable(boolean scrollable) {
+        this.scrollable = scrollable;
+    }
+}
